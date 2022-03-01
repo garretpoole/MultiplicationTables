3cc2e31b8f6b81604330fdd06fbbd74a593df6dc
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-28 21:06:31 -0800

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Garret Poole on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
