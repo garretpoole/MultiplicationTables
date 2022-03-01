59ba49792ea653e7f342e0dff2de50c2d21146d7
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-01 10:52:12 -0800

    settings page in content view; shows based on bool

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -7,14 +7,98 @@
 
 import SwiftUI
 
+//struct Settings: View{
+//    @State private var timesTable = 2
+//    @State private var totalQuestions = 5
+//    @State private var difficulty = ""
+//
+//    let possibleQuestions = [5, 10, 15, 20]
+//    let difficulties = ["easy", "hard"]
+//    var body: some View{
+//        NavigationView{
+//            Form{
+//                Section("Select Tables"){
+//                    Stepper("Tables up to...\(timesTable.formatted())", value: $timesTable, in: 2...12)
+//                }
+//                Section("Total Questions"){
+//                    Picker("Number of Questions", selection: $totalQuestions){
+//                        ForEach(possibleQuestions, id: \.self){
+//                            Text("\($0)")
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                }
+//                Section("Difficulty"){
+//                    Picker("Difficulty", selection: $difficulty){
+//                        ForEach(difficulties, id: \.self){
+//                            Text($0)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                }
+//            }
+//            .navigationTitle("Settings")
+//        }
+//    }
+//}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State private var showSettings = true
+    
+    @State private var timesTable = 2
+    @State private var totalQuestions = 5
+    @State private var difficulty = "easy"
+    
+    let possibleQuestions = [5, 10, 15, 20]
+    let difficulties = ["easy", "hard"]
+    var body: some View{
+        if showSettings{
+            NavigationView{
+                Form{
+                    Section("Select Tables"){
+                        Stepper("Tables up to...\(timesTable.formatted())", value: $timesTable, in: 2...12)
+                    }
+                    Section("Total Questions"){
+                        Picker("Number of Questions", selection: $totalQuestions){
+                            ForEach(possibleQuestions, id: \.self){
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Section("Difficulty"){
+                        Picker("Difficulty", selection: $difficulty){
+                            ForEach(difficulties, id: \.self){
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Button("Confirm"){
+                        showSettings = false
+                    }
+                }
+                .navigationTitle("Settings")
+            }
+        }
+        else{
+            NavigationView{
+                VStack{
+                    
+                }
+                .navigationTitle("Multiply")
+                .toolbar {
+                    Button("Settings"){
+                        showSettings = true
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
