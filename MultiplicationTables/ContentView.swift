7504e8e1a215216b4a158798bb02cdaaed178f43
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-07 15:09:55 -0800

    Overall functionality complete; 3 total views, tracks right answers

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -54,6 +54,15 @@ struct ContentView: View {
     @State private var difficulty = "easy"
     @State private var questions = [Question]()
     
+    @State private var currentQuestion = 0
+    @State private var answer = 0
+    @State private var correct = 0
+    @FocusState private var answerFocused: Bool
+    
+    @State private var showingSubmission = false
+    @State private var submissionTitle = ""
+    @State private var submissionMessage = ""
+    
     let possibleQuestions = [5, 10, 15, 20]
     let difficulties = ["easy", "hard"]
     var body: some View{
@@ -86,19 +95,83 @@ struct ContentView: View {
                 .navigationTitle("Settings")
             }
         }
-        else{
+        else if currentQuestion < totalQuestions{
             NavigationView{
-                VStack{
+                List{
+                    ForEach(0..<questions.count, id: \.self) { i in
+                        if i == currentQuestion {
+                            Text(questions[i].question)
+                        }
+                    }
+                    Section{
+                        TextField("Answer", value: $answer, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($answerFocused)
+                    }
+                    
                     
                 }
                 .navigationTitle("Multiply")
                 .onAppear(perform: generateQuestions)
                 .toolbar {
                     Button("Settings"){
-                        showSettings = true
+                        resetGame()
                     }
                 }
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard){
+                        Spacer()
+                        Button("Submit"){
+                            answerFocused = false
+                            checkAnswer()
+                        }
+                    }
+                }
+                .alert(submissionTitle, isPresented: $showingSubmission){
+                    Button("OK", role: .cancel){
+                        currentQuestion += 1
+                    }
+                } message: {
+                    Text(submissionMessage)
+                }
             }
+        }
+        else{
+            NavigationView{
+                VStack{
+                    Text("Times Tables up to: \(timesTable)")
+                    Text("Difficulty: " + difficulty)
+                    Text("Total Questions: \(questions.count)")
+                    Text("Correct: \(correct)")
+                    Button("Continue"){
+                        resetGame()
+                    }
+                    .padding()
+                    .clipped()
+                    .background(.red)
+                    .foregroundColor(.white)
+                }
+                .navigationTitle("COMPLETE")
+            }
+        }
+    }
+    func resetGame() {
+        showSettings = true
+        currentQuestion = 0
+        answer = 0
+        correct = 0
+    }
+    
+    func checkAnswer(){
+        showingSubmission = true
+        if answer == questions[currentQuestion].answer {
+            correct += 1
+            submissionTitle = "CORRECT"
+            submissionMessage = questions[currentQuestion].question + "= \(questions[currentQuestion].answer)"
+        }
+        else{
+            submissionTitle = "WRONG"
+            submissionMessage = questions[currentQuestion].question + "= \(questions[currentQuestion].answer)"
         }
     }
     
@@ -106,12 +179,15 @@ struct ContentView: View {
         questions = [Question]()
         var rand1: Int
         var rand2: Int
+        var easyRange = (1...10)
+        var hardRange = (10...100)
+        
         switch difficulty{
         case "easy":
             for _ in (0..<totalQuestions) {
                 var tempQuestion = Question()
                 rand1 = Int.random(in: 2...timesTable)
-                rand2 = Int.random(in: 1...10)
+                rand2 = Int.random(in: easyRange)
                 tempQuestion.question = "\(rand1) * \(rand2)"
                 tempQuestion.answer = rand1 * rand2
                 questions.append(tempQuestion)
